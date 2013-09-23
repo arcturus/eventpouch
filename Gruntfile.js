2c4788a7fb57ec1102fbf5bb0c4b11fa1c871a40
Francisco Jordano <arcturus@ardeenelinfierno.com>
2013-09-23 23:02:00 +0100

    Basic grunt task added

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,10 +1,34 @@
 'use strict';
 
 module.exports = function (grunt) {
-  // load jshint plugin
+
   grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-qunit');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-concat');
 
   grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    concat: {
+      options: {
+        separator: ';'
+      },
+      dist: {
+        src: ['src/*.js'],
+        dest: 'dist/<%= pkg.name %>-<%= pkg.version %>.js'
+      }
+    },
+    uglify: {
+      options: {
+        banner: '/*! <%= pkg.name %> <%= grunt.template.today("dd-mm-yyyy") %> */\n'
+      },
+      dist: {
+        files: {
+          'dist/<%= pkg.name %>-<%= pkg.version %>.min.js': ['<%= concat.dist.dest %>']
+        }
+      }
+    },
     jshint: {
       options: {
         node: true
@@ -13,6 +37,12 @@ module.exports = function (grunt) {
         'Gruntfile.js',
         'src/*.js'
       ]
+    },
+    watch: {
+      files: ['<%= jshint.all %>'],
+      tasks: ['jshint']
     }
   });
+
+  grunt.registerTask('default', ['jshint', 'concat', 'uglify']);
 };
