1f45424ddadd3049bbb65564a7e68ac1ef7fd90f
Francisco Jordano <arcturus@ardeenelinfierno.com>
2013-09-25 17:06:54 +0100

    Removing unused plugin

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,7 +4,6 @@ module.exports = function (grunt) {
 
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-contrib-qunit');
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-contrib-concat');
 
